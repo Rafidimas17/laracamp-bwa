9594e1cfd78110d913b7a6cbe808a1f2b739a8b9
Rafidimas17 <rafidimas1706@gmail.com>
2023-09-26 01:09:26 +0700

    finish migration camp, camp_benefits, and checkout

--- database/migrations/2023_09_25_175450_create_camp_benefits_table.php
+++ database/migrations/2023_09_25_175450_create_camp_benefits_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProducts extends Migration
+class CreateCampBenefitsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,10 @@ class CreateProducts extends Migration
      */
     public function up()
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('camp_benefits', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger("product_category_id");
-            $table->string("name");
-            $table->text("description");
+            $table->foreignId('camp_id')->constrained();
+            $table->string('name');
             $table->timestamps();
         });
     }
@@ -29,6 +28,6 @@ class CreateProducts extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('camp_benefits');
     }
 }
